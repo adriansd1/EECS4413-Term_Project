599276a18533af46d589e7e329f8fa4b0db64f3e
Adrian Sam Daliri <154755212+adriansd1@users.noreply.github.com>
2025-10-14 21:35:48 -0400

    First commit of file structure

---
+++ src/main/java/org/eecs4413/eecs4413term_project/Eecs4413TermProjectApplication.java
@@ -0,0 +1,13 @@
+package org.eecs4413.eecs4413term_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eecs4413TermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eecs4413TermProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/eecs4413/eecs4413term_project/Eecs4413TermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eecs4413.eecs4413term_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eecs4413TermProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
