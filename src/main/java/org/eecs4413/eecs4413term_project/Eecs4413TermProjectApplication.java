fc58e128793abddbf2f17a060cb70be8cb3e2b55
Ayan Saiyed <ayansaiyed@mac.home>
2025-10-26 21:12:28 -0400

    fixing gradle build issues

--- src/main/java/org/eecs4413/eecs4413term_project/Eecs4413TermProjectApplication.java
+++ src/main/java/org/eecs4413/eecs4413term_project/Eecs4413TermProjectApplication.java
@@ -1,14 +1,15 @@
 package org.eecs4413.eecs4413term_project;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class Eecs4413TermProjectApplication {
 
     public static void main(String[] args) {
         System.out.println("Starting EECS4413 Term Project Application...");
-        SpringApplication.run(Eecs4413TermProjectApplication.class, args);
+        org.springframework.boot.SpringApplication.run(Eecs4413TermProjectApplication.class, args);
     }
 
 }
